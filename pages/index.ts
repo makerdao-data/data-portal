16dc3810c0159fed861e8ef3d609f008fc1f8d5b
Emmett Brown <nioto@lighthouseprojects.cx>
2022-11-21 13:03:25 -0300

    Fix path

--- pages/index.ts
+++ pages/index.ts
@@ -4,7 +4,7 @@ export async function getStaticProps({ locale }: { locale: string }) {
       // You can get the messages from anywhere you like. The recommended
       // pattern is to put them in JSON files separated by language and read
       // the desired one based on the `locale` received from Next.js.
-      messages: (await import(`../../i18n/messages/${locale}.json`)).default
+      messages: (await import(`../i18n/messages/${locale}.json`)).default
     }
   };
 }
