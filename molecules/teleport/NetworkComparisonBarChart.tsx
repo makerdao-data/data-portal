436ecf62d534d90d9554525829989b45a040cb47
Emmett Brown <nioto@lighthouseprojects.cx>
2023-01-09 11:49:52 +0100

    Replace barchart for pie charts in network comparison

--- molecules/teleport/NetworkComparisonBarChart.tsx
+++ molecules/teleport/NetworkComparisonBarChart.tsx
@@ -1,4 +1,4 @@
-import { ResponsiveBar } from '@nivo/bar';
+import { ResponsivePie } from '@nivo/pie';
 import { useIntl } from 'next-intl';
 import { Box, Flex, useColorMode } from 'theme-ui';
 import {
@@ -7,7 +7,7 @@ import {
   NetworkComparisonIndex
 } from '../../transformers/create-network-comparison-chart-data-series';
 import { Summary } from '../../__generated__/dataAPI';
-import { Text, useTheme } from '@makerdao-dicu/makerdao-ui';
+import { Text } from '@makerdao-dicu/makerdao-ui';
 import { Fragment, useMemo } from 'react';
 import Skeleton from 'react-loading-skeleton';
 
@@ -22,7 +22,6 @@ export default function NetworkComparisonBarChart({
 }: NetworkComparitionBarChartProps) {
   const intl = useIntl();
   const [colorMode] = useColorMode();
-  const { theme } = useTheme();
   const dataSeries = useMemo(
     () => createNetworkComparisonChartDataSeries(data),
     [data]
@@ -38,7 +37,7 @@ export default function NetworkComparisonBarChart({
         border: error ? 'none' : '1px solid',
         borderColor: 'secondary',
         borderRadius: '8px',
-        height: '260px',
+        // height: ['100%', '100%', '260px'],
         flex: ['1 1 100%', '1 1 100%', '1 1 100%', '1 1 0%'],
         alignSelf: 'flex-end',
         padding: 2
@@ -55,160 +54,90 @@ export default function NetworkComparisonBarChart({
           <Text variant="error">
             {'Network comparison data is not available at the moment.'}
           </Text>
-        ) : dataSeries.length > 0 ? (
-          <ResponsiveBar
-            data={dataSeries}
-            role="figure"
-            ariaLabel="Network comparison chart"
-            barAriaLabel={function (e) {
-              return (
-                e.id + ': ' + e.formattedValue + ' in domain: ' + e.indexValue
-              );
-            }}
-            keys={['optimism', 'arbitrum', 'starknet']}
-            indexBy="id"
-            margin={{ top: 20, right: 100, bottom: 50, left: 50 }}
-            padding={0.3}
-            valueScale={{ type: 'linear' }}
-            valueFormat={(value) =>
-              intl.formatNumber(value, {
-                style: 'percent',
-                maximumFractionDigits: 2,
-                notation: 'compact'
-              })
-            }
-            labelFormat={(value) =>
-              intl.formatNumber(Number(value) * 1000, {
-                maximumFractionDigits: 2,
-                notation: 'compact'
-              })
-            }
-            theme={{
-              textColor: colorMode === 'light' ? '#231536' : '#fff',
-              legends: {
-                text: {
-                  fontSize: theme.fontSizes?.[3]
-                }
-              },
-              labels: {
-                text: {
-                  fontSize: theme.fontSizes?.[4],
-                  fontWeight: 500
-                }
-              },
-              axis: {
-                ticks: {
-                  text: {
-                    fontSize: theme.fontSizes?.[4]
-                  }
-                }
-              },
-              grid: {
-                line: {
-                  strokeWidth: 0.5
-                }
-              }
-            }}
-            indexScale={{ type: 'band', round: true }}
-            colors={({ id, data }) => String(data[id + 'Color'])}
-            borderColor={colorMode === 'light' ? '#231536' : '#fff'}
-            borderWidth={1}
-            borderRadius={5}
-            axisTop={null}
-            axisRight={null}
-            enableGridY={false}
-            axisBottom={{
-              tickSize: 5,
-              tickPadding: 5,
-              tickRotation: 0,
-              legendPosition: 'middle',
-              legendOffset: 32,
-              format: (value: keyof typeof NetworkComparisonIndex) =>
-                NetworkComparisonIndex[value]
-            }}
-            axisLeft={{
-              tickSize: 5,
-              tickPadding: 5,
-              tickRotation: 0,
-              legendPosition: 'middle',
-              legendOffset: -40,
-              format: (value) => intl.formatNumber(value, { style: 'percent' })
-            }}
-            labelSkipWidth={12}
-            labelSkipHeight={12}
-            labelTextColor="#fff"
-            legends={[
-              {
-                dataFrom: 'keys',
-                data: [
-                  {
-                    id: 'optimism',
-                    label: 'Optimism',
-                    color: domainColor.optimism
-                  },
-                  {
-                    id: 'arbitrum',
-                    label: 'Arbitrum',
-                    color: domainColor.arbitrum
-                  },
-                  {
-                    id: 'starknet',
-                    label: 'Starknet',
-                    color: domainColor.starknet
-                  }
-                ],
-                anchor: 'bottom-right',
-                direction: 'column',
-                justify: false,
-                translateX: 120,
-                translateY: 0,
-                itemsSpacing: 2,
-                itemWidth: 100,
-                itemHeight: 20,
-                itemDirection: 'left-to-right',
-                itemOpacity: 0.85,
-                symbolSize: 15,
-                effects: [
-                  {
-                    on: 'hover',
-                    style: {
-                      itemOpacity: 1
+        ) : Object.values(dataSeries).every((value) => value.length) ? (
+          <Flex sx={{ gap: 2, flexWrap: ['wrap', 'wrap', 'nowrap'] }}>
+            {Object.entries(dataSeries).map(([key, data]) => (
+              <Flex
+                key={key}
+                sx={{
+                  flexDirection: 'column',
+                  height: '220px',
+                  width: '100%'
+                }}>
+                <Box sx={{ height: '90%' }}>
+                  <ResponsivePie
+                    data={data}
+                    id={({ formattedId }) => formattedId}
+                    margin={{ top: 40, right: 0, bottom: 20, left: 0 }}
+                    innerRadius={0.5}
+                    padAngle={0.7}
+                    cornerRadius={3}
+                    activeOuterRadiusOffset={8}
+                    colors={[
+                      domainColor.optimism,
+                      domainColor.arbitrum,
+                      domainColor.starknet
+                    ]}
+                    borderWidth={1}
+                    borderColor={{
+                      from: 'color',
+                      modifiers: [['darker', 0.2]]
+                    }}
+                    valueFormat={(value) =>
+                      intl.formatNumber(value, {
+                        style: 'percent',
+                        maximumFractionDigits: 0,
+                        notation: 'compact'
+                      })
                     }
-                  }
-                ]
-              }
-            ]}
-            tooltip={(props) => {
-              return (
-                <Flex
-                  as="div"
-                  sx={{
-                    backgroundColor: 'background',
-                    padding: 2,
-                    border: '1px solid',
-                    borderColor: 'secondary',
-                    borderRadius: '5px',
-                    gap: 1
-                  }}>
-                  <Box
-                    as="div"
-                    sx={{
-                      width: '15px',
-                      height: '15px',
-                      backgroundColor: props.color,
-                      alignSelf: 'center'
+                    arcLinkLabelsSkipAngle={1}
+                    arcLinkLabelsThickness={2}
+                    arcLinkLabelsColor={{ from: 'color' }}
+                    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                    // @ts-ignore
+                    arcLinkLabel={({ data }) => data.formattedId}
+                    arcLabelsSkipAngle={15}
+                    arcLabelsTextColor={{
+                      from: 'color',
+                      modifiers: [['darker', 10]]
+                    }}
+                    theme={{
+                      textColor: colorMode === 'light' ? '#231536' : '#fff'
+                    }}
+                    tooltip={({ datum }) => {
+                      return (
+                        <Flex
+                          as="div"
+                          sx={{
+                            backgroundColor: 'background',
+                            padding: 2,
+                            border: '1px solid',
+                            borderColor: 'secondary',
+                            borderRadius: '5px',
+                            gap: 1
+                          }}>
+                          <Text variant="boldBody">{`${
+                            datum.data.formattedId
+                          } ${intl.formatNumber(datum.data.value, {
+                            style: 'percent',
+                            maximumFractionDigits: 2,
+                            notation: 'compact'
+                          })}`}</Text>
+                        </Flex>
+                      );
                     }}
                   />
-                  <Text>
-                    {(props.id as string)[0].toUpperCase() +
-                      (props.id as string).split('').splice(1).join('') +
-                      ':'}
-                  </Text>
-                  <Text variant="boldBody">{props.formattedValue}</Text>
-                </Flex>
-              );
-            }}
-          />
+                </Box>
+                <Text variant="smallText" sx={{ textAlign: 'center' }}>
+                  {
+                    NetworkComparisonIndex[
+                      key as keyof typeof NetworkComparisonIndex
+                    ]
+                  }
+                </Text>
+              </Flex>
+            ))}
+          </Flex>
         ) : (
           <Skeleton height={220} style={{ borderRadius: '8px', top: '-4px' }} />
         )}
