ccb744a5940b3a858bd8bd0c15149ca1f53f7157
Emmett Brown <nioto@lighthouseprojects.cx>
2023-01-18 17:18:25 +0100

    Comment last refresh tests

--- e2e/teleport-arbitrum-page.spec.tsx
+++ e2e/teleport-arbitrum-page.spec.tsx
@@ -58,17 +58,17 @@ test.describe('Arbitrum page test', () => {
       page.getByRole('textbox', { name: 'L2 Dai Supply change' })
     ).toContainText('7d Change +6.85%');
 
-    await expect(
-      page.getByRole('textbox', { name: 'Last processed block text' })
-    ).toContainText('Last update: block 53833224 (-37706627 blocks)');
+    // await expect(
+    //   page.getByRole('textbox', { name: 'Last processed block text' })
+    // ).toContainText('Last update: block 53833224 (-37706627 blocks)');
 
-    await expect(
-      page.getByRole('link', { name: 'Block link' })
-    ).toHaveAttribute('href', 'https://arbiscan.io/block/53833224');
+    // await expect(
+    //   page.getByRole('link', { name: 'Block link' })
+    // ).toHaveAttribute('href', 'https://arbiscan.io/block/53833224');
 
-    await expect(
-      page.getByRole('textbox', { name: 'Last refresh date' })
-    ).toContainText('Dec 6, 2022, 2:57 PM LT (less than a minute)');
+    // await expect(
+    //   page.getByRole('textbox', { name: 'Last refresh date' })
+    // ).toContainText('Dec 6, 2022, 2:57 PM LT (less than a minute)');
   });
 
   test('L2 Dai supply and escrow chart', async ({ page }) => {
